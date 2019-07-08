b614c1421bfa35486e7c5f618c24a37a1e6f740e
LuanSir <625322698@qq.com>
2019-07-08 22:52:18 +0800

    请求登录

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,19 +2,36 @@
   <div>
     <van-nav-bar class="nav-bar" title="登录" />
     <van-cell-group>
-      <van-field v-model="user.mobile" required clearable label="用户名" placeholder="请输入手机号" />
+      <van-field
+        v-model="user.mobile"
+        class="phone"
+        clearable
+        left-icon="friends-o"
+        label="用户名"
+        placeholder="请输入手机号"
+      />
       <!-- <van-field v-model="user.code" type="password" label="验证码" placeholder="请输入验证码" required /> -->
-      <van-field v-model="user.code" center clearable label="短信验证码" placeholder="请输入短信验证码">
+      <van-field
+        v-model="user.code"
+        class="pwd"
+        center
+        clearable
+        left-icon="phone-o"
+        label="短信验证码"
+        placeholder="请输入短信验证码"
+      >
         <van-button slot="button" size="small" type="primary">发送验证码</van-button>
       </van-field>
     </van-cell-group>
     <div>
-      <van-button class="btn" type="info" @click.native="handleLogin">登录</van-button>
+      <van-button class="btn" type="info" @click.prevent="handleLogin">登录</van-button>
     </div>
   </div>
 </template>
 
 <script>
+// 引入axios
+import axios from 'axios'
 export default {
   name: 'Login',
   data () {
@@ -24,6 +41,16 @@ export default {
         code: ''
       }
     }
+  },
+  methods: {
+    async handleLogin () {
+      const res = await axios({
+        method: 'POST',
+        url: 'http://toutiao.course.itcast.cn/app/v1_0/authorizations',
+        data: this.user
+      })
+      console.log(res)
+    }
   }
 }
 </script>
@@ -32,7 +59,14 @@ export default {
 .nav-bar {
   background: #3296fa;
 }
+.phone {
+  font-size: 16px;
+}
+.pwd {
+  font-size: 16px;
+}
 .btn {
   width: 100%;
+  font-size: 16px;
 }
 </style>
